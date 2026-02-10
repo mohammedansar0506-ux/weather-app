5977591b53d2f4af32bf5d1cfe56a859f6c481dd
mohammedansar0506-ux <mohammedansar0506@gmail.com>
2026-02-10 17:40:47 +0530

    customised css and added extra weather data

--- script.js
+++ script.js
@@ -4,24 +4,27 @@ let result = document.getElementById('result');
 async function fetchWeather() {
     const city = cityName.value;
     if (city === "") {
-        result.innerHTML = 'Invalid Input'
+        result.innerHTML = 'City name cannot be empty.'
     }
     else {
         try {
-            result.innerHTML = 'Loading...'
+            result.innerHTML = 'Getting latest weather...'
             const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=84f4e950d23f50aec1107a3944bb936c`)
             if (response.ok) {
                 const data = await response.json();
                 console.log(data);
                 const weatherData = data.main;
                 const weather = Math.round(weatherData.temp - 273.15);
-                const climate = data.weather[0].description.toUpperCase();
-                result.innerHTML = `${weather}°C, ${climate}`;
+                const climate = data.weather[0].main;
+                result.innerHTML = `
+                <div class="weather-result">
+                    <p>${data.name}, ${data.sys.country}</p>
+                    <h1>${weather}°C</h1>
+                    <p>${climate}</p>
+                </div>`
             }
-            else {
-                result.innerHTML = 'Incorrect City';
-            }
+            else { result.innerHTML = 'City not found. <br> Check the spelling or try adding a country code (e.g. Rome, IT)'; }
         }
-        catch { result.innerHTML = 'No Internet Connection' };
+        catch { result.innerHTML = 'Network error. Check your connection.' };
     }
 } 
