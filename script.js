757506a808f71897b7098374ee39ebdf1fec99c5
mohammedansar0506-ux <mohammedansar0506@gmail.com>
2026-02-19 19:41:01 +0530

    minor style changes

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
-const cityName = document.getElementById('cityName');
-let result = document.getElementById('result');
+const cityName = document.getElementById("cityName");
+let result = document.getElementById("result");
 
 async function fetchWeather() {
-    const city = cityName.value;
-    if (city === "") {
-        result.innerHTML = 'City name cannot be empty.'
-    }
-    else {
-        try {
-            result.innerHTML = 'Getting latest weather...'
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=84f4e950d23f50aec1107a3944bb936c`)
-            if (response.ok) {
-                const data = await response.json();
-                console.log(data);
-                const weatherData = data.main;
-                const weather = Math.round(weatherData.temp - 273.15);
-                const climate = data.weather[0].main;
-                result.innerHTML = `
+  const city = cityName.value;
+  if (city !== "") {
+    try {
+      result.innerHTML = "Getting latest weather...";
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=84f4e950d23f50aec1107a3944bb936c`,
+      );
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        const weatherData = data.main;
+        const weather = Math.round(weatherData.temp - 273.15);
+        const climate = data.weather[0].main;
+        result.innerHTML = `
                 <div class="weather-result">
                     <p>${data.name}, ${data.sys.country}</p>
                     <h1>${weather}°C</h1>
                     <p>${climate}</p>
-                </div>`
-            }
-            else { result.innerHTML = 'City not found. <br> Check the spelling or try adding a country code (e.g. Rome, IT)'; }
-        }
-        catch { result.innerHTML = 'Network error. Check your connection.' };
+                </div>`;
+      } else {
+        result.innerHTML =
+          "City not found. <br> Check the spelling or try adding a country code (e.g. Rome, IT)";
+      }
+    } catch {
+      result.innerHTML = "Network error. Check your connection.";
     }
-} 
+  } else {
+    result.innerHTML = "City name cannot be empty.";
+  }
+}
